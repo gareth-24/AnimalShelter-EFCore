0af3e04e9b3c3df9c4335a214766b8fc0e4c9faa
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-16 16:46:46 -0700

    add updated files

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalShelter.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Animals.ToList(); //order by id
+      List<Animal> model2 = _db.Animals.OrderBy(animal => animal.Name).ToList(); //order alphabetically
+      return View(model2); 
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+      return View(thisAnimal);
+    }
+  }
+}
